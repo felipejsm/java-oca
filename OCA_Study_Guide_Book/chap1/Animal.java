8f1021cec637f542ca5063a52c131ab374d4bd9a
Felipe Moysés <felipe.moyses@techne.com.br>
2017-11-09 16:19:01 -0200

    An example of basic Java

--- OCA_Study_Guide_Book/chap1/Animal.java
+++ OCA_Study_Guide_Book/chap1/Animal.java
@@ -1,1 +1,14 @@
-public class Animal {}
+public class Animal {
+    String name;
+    //method signature: full declaration of a method
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+class Animal2 {
+    
+}
